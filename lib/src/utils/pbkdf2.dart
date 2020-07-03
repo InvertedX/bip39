5547f27c16ed1adb221b91245fabf20102de2687
Sarath <sarathkumars677@gmail.com>
2020-07-03 14:08:05 +0530

    Passphrase as parameter

--- lib/src/utils/pbkdf2.dart
+++ lib/src/utils/pbkdf2.dart
@@ -6,24 +6,18 @@ import 'package:pointycastle/key_derivators/pbkdf2.dart';
 import 'package:pointycastle/macs/hmac.dart';
 
 class PBKDF2 {
-  final int blockLength;
-  final int iterationCount;
-  final int desiredKeyLength;
-
-  PBKDF2KeyDerivator _derivator;
-  Uint8List _salt;
-
-  PBKDF2({this.blockLength = 128,
-          this.iterationCount = 2048,
-          this.desiredKeyLength = 64,
-          String salt = "mnemonic"}) {
-    _salt = utf8.encode(salt);
-    _derivator =
-    new PBKDF2KeyDerivator(new HMac(new SHA512Digest(), blockLength))
-      ..init(new Pbkdf2Parameters(_salt, iterationCount, desiredKeyLength));
-  }
-
-  Uint8List process(String mnemonic) {
+  static Uint8List process(
+    String mnemonic, {
+    passPhrase = "",
+    blockLength = 128,
+    iterationCount = 2048,
+    desiredKeyLength = 64,
+  }) {
+    String salt = "mnemonic${passPhrase}";
+    Uint8List _salt = utf8.encode(salt);
+    PBKDF2KeyDerivator _derivator =
+        new PBKDF2KeyDerivator(new HMac(new SHA512Digest(), blockLength))
+          ..init(new Pbkdf2Parameters(_salt, iterationCount, desiredKeyLength));
     return _derivator.process(new Uint8List.fromList(mnemonic.codeUnits));
   }
 }
