1b730c10693bf20e85e4efad054cc7afe6ed6e35
Sarath <sarathkumars677@gmail.com>
2020-07-03 14:18:28 +0530

    Examples

--- example/bip39_example.dart
+++ example/bip39_example.dart
@@ -1,16 +1,28 @@
-import 'package:bip39/bip39.dart' as bip39;
+import 'package:bip39/bip39.dart';
 
 main() async {
-  String randomMnemonic = bip39.generateMnemonic();
-  print(randomMnemonic);
-  String seed = bip39.mnemonicToSeedHex("update elbow source spin squeeze horror world become oak assist bomb nuclear");
-  print(seed);
-  String mnemonic = bip39.entropyToMnemonic('00000000000000000000000000000000');
-  print(mnemonic);
-  bool isValid = bip39.validateMnemonic(mnemonic);
-  print(isValid);
-  isValid = bip39.validateMnemonic('basket actual');
-  print(isValid);
-  String entropy = bip39.mnemonicToEntropy(mnemonic);
-  print(entropy);
+  /**
+   * Generate random mnemonic
+   */
+  Bip39 randomMnemonic = Bip39().generateMnemonic();
+  print("Seed Mnemonic: ${randomMnemonic.getMnemonic()}");
+  print("Seed : ${randomMnemonic.toSeed()}");
+  print("Seed HEX: ${randomMnemonic.toSeedHEX()}");
+  print("Seed Entropy: ${randomMnemonic.getEntropy()}");
+
+  print("\n\n");
+
+  Bip39 provideMnemonic = Bip39(
+      mnemonic:
+          "club middle wheat river state exile embark excuse syrup teach trap fruit");
+  print("Seed for given mnemonic: ${provideMnemonic.toSeed()} ");
+  print("Seed HEX for given mnemonic: ${provideMnemonic.toSeedHEX()} ");
+  print("Seed HEX for given mnemonic with password: ${provideMnemonic.toSeedHEX(passPhrase: "qwerty")} ");
+
+  print("\n\n");
+
+  /**
+   * Validating mnemonic
+   */
+  print("Validate : ${Bip39.validateMnemonic('club excuse syrup teach trap fruit')}");
 }
